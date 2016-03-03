8a950d6a3c8a1dc309c21c80937627b3b93ec541
briez <briez@bk.ru>
2016-03-03 13:24:03 +0300

    I added Hello World

--- TestApplication/Program.cs
+++ TestApplication/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace TestApplication
 {
@@ -10,6 +6,7 @@ namespace TestApplication
     {
         static void Main(string[] args)
         {
+            System.Console.WriteLine("Hello World");
         }
     }
 }
